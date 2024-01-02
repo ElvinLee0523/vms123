7bc2fd7e6abcab57eb82ed4558a1893ec6aae930
ElvinLee0523 <elvinlee0523@gmail.com>
2024-01-02 11:22:33 +0800

    cibai

--- swagger.js
+++ swagger.js
@@ -339,6 +339,8 @@
  *           application/json:
  *             example:
  *               message: "Registration request processed, visitor is Visitor Name"
+ *       400:
+ *         description: Bad Request. Visitor with the provided reference number already exists.
  *       401:
  *         description: Unauthorized. Token not valid.
  *       500:
